476bef56c22ef926c720e42b3804d30fd996f63a
benny-sit <108618452+benny-sit@users.noreply.github.com>
2023-01-22 15:06:01 +0200

    v1 fix img

--- src/Components/HeroContainer.jsx
+++ src/Components/HeroContainer.jsx
@@ -9,7 +9,7 @@ export default function HeroContainer() {
   return (
     <div className=' max-w-4xl w-full backdrop-blur-xs mx-auto h-full relative'>
         <Parallax scale={[1.1, 0.6]} translateX={[-50, 0]} speed={10} rotate={[-10, 20]}  className='absolute right-0 top-0 lg:top-20  max-h-full'>
-            <img className='object-fit max-h-full' src="/feather.png" alt="" />
+            <img className='object-fit max-h-full' src="/assets/feather.png" alt="feather 1" />
         </Parallax>
         <HeroText />
     </div>
